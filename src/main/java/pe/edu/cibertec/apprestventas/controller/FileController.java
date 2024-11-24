cd2c9a4eab549796905e06e4b4751be01282eb87
Grupo5CicloWeb <cicloc504@gmail.com>
2024-11-24 11:21:58 -0500

    subida de archivos

---
+++ src/main/java/pe/edu/cibertec/apprestventas/controller/FileController.java
@@ -0,0 +1,32 @@
+package pe.edu.cibertec.apprestventas.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import pe.edu.cibertec.apprestventas.dto.ArchivoDto;
+import pe.edu.cibertec.apprestventas.service.FileService;
+import pe.edu.cibertec.apprestventas.service.IFileService;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1/file")
+public class FileController {
+    private final IFileService fileService;
+
+    @PostMapping
+    public ResponseEntity<ArchivoDto> subirArchivos(
+            @RequestParam("files") List<MultipartFile> files
+    ) throws Exception{
+        fileService.guardarArchivos(files);
+        return ResponseEntity.ok().build();
+    }
+
+
+
+}
---
+++ src/main/java/pe/edu/cibertec/apprestventas/dto/ArchivoDto.java
@@ -0,0 +1,10 @@
+package pe.edu.cibertec.apprestventas.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+@Data
+public class ArchivoDto {
+    private String mensaje;
+}
